10737bf86005f43d0ee47c8cded0fdb7644bc0fa
Oleksii Kurii <olekseykuriy@gmail.com>
2026-04-24 14:32:43 +0300

    created booking page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Booking from "./pages/Booking";
 
 export default function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking/:id" element={<Booking />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
