14ca410c931a6c481042dda75ad272916ec60ea1
Oleksii Kurii <olekseykuriy@gmail.com>
2026-04-24 14:16:51 +0300

    added train data

---
+++ src/data/trains.js
@@ -0,0 +1,16 @@
+export const trains = [
+  {
+    id: 1,
+    number: "IC101",
+    route: "Львів - Київ",
+    time: "08:00",
+    duration: "5 год",
+  },
+  {
+    id: 2,
+    number: "IC202",
+    route: "Київ - Харків",
+    time: "12:00",
+    duration: "4 год",
+  },
+];
